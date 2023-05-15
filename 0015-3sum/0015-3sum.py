0e7d59e2ae34a3b5d34f6831adbd43a38593eda2
Skye Helfant <111463078+Skye-Helfant@users.noreply.github.com>
2023-05-15 20:15:55 +0200

    Time: 2787 ms (25.08%), Space: 20.4 MB (20.61%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,24 +1,30 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()  # Sort the list to simplify the algorithm
+        nums.sort()
+        left, middle, right = 0, 1, len(nums) - 1
         triplets = []
-        for i in range(len(nums)-2):
-            if i > 0 and nums[i] == nums[i-1]:
-                continue  # Skip duplicates
-            left, right = i+1, len(nums)-1
-            while left < right:
-                s = nums[i] + nums[left] + nums[right]
-                if s < 0:
+
+        while left < len(nums) - 2:
+            s = nums[left] + nums[middle] + nums[right]
+            if s == 0:
+                triplets.append([nums[left], nums[middle], nums[right]])
+                # Skip over all duplicates of left, middle, and right
+                while left < len(nums) - 2 and nums[left] == nums[left+1]:
                     left += 1
-                elif s > 0:
+                while middle < right and nums[middle] == nums[middle+1]:
+                    middle += 1
+                while middle < right and nums[right] == nums[right-1]:
                     right -= 1
-                else:
-                    triplets.append([nums[i], nums[left], nums[right]])
-                    left += 1
-                    right -= 1
-                    # Skip duplicates
-                    while left < right and nums[left] == nums[left-1]:
-                        left += 1
-                    while left < right and nums[right] == nums[right+1]:
-                        right -= 1
+                # Move middle and right pointers
+                middle += 1
+                right -= 1
+            elif s > 0:
+                right -= 1
+            else:
+                middle += 1
+            if middle >= right or right >= len(nums):
+                left += 1
+                middle = left + 1
+                right = len(nums) - 1
+
         return triplets
