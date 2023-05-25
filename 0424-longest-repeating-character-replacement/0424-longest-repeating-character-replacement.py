b0300588ac3270d06fa39f68f4b567853bc8782b
Skye Helfant <111463078+Skye-Helfant@users.noreply.github.com>
2023-05-25 08:50:55 +0200

    Time: 181 ms (27.03%), Space: 16.3 MB (37.61%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,16 +1,25 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        # LEETCODE explanation assistance
         count = {}
         sol = 0
-
         l = 0
+        
         for r in range(len(s)):
             count[s[r]] = 1 + count.get(s[r], 0)
-
+            # print("Window: ", s[l:r+1])
+            # print("Count: ", count)
+            # print("Max Count: ", max(count.values()))
+            
             while (r - l + 1) - max(count.values()) > k:
                 count[s[l]] -= 1
                 l += 1
-
+                # print("Shrinking window...")
+                # print("Window: ", s[l:r+1])
+                # print("Count: ", count)
+                # print("Max Count: ", max(count.values()))
+            
             sol = max(sol, r - l + 1)
+            # print("Current longest substring length: ", sol)
+            # print("-------------------")
+        
         return sol
