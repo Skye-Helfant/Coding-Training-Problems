3afe5b2508807aa2f38642d8eda30106dc3b4b31
Skye Helfant <111463078+Skye-Helfant@users.noreply.github.com>
2023-05-27 11:40:49 +0200

    Time: 257 ms (9.54%), Space: 17.9 MB (21.73%) - LeetHub

--- 0792-binary-search/0792-binary-search.py
+++ 0792-binary-search/0792-binary-search.py
@@ -3,13 +3,12 @@ class Solution:
         l, r = 0, len(nums) - 1
 
         while l <= r:
-            i = (l + r) // 2  # Use (l + r) // 2 to calculate the middle index correctly
+            m = (l + r) // 2
 
-            if target < nums[i]:
-                r = i - 1  # Update r to i - 1 when target is smaller
-            elif target > nums[i]:
-                l = i + 1  # Update l to i + 1 when target is larger
+            if nums[m] > target:
+                r = m - 1
+            elif nums[m] < target:
+                l = m + 1
             else:
-                return i  # Return the index if target is found
-
-        return -1  # Return -1 if target is not found
+                return m
+        return -1
